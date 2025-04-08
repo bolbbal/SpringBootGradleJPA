881905b8438f0f0e051bb30c0e4af66b3ebc037b
Yongwook Choi <dyddnr25@gmail.com>
2025-04-08 22:47:11 +0900

    feat
    add layout & fragment

---
+++ board/src/main/java/com/board/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping("")
+    public String main(Model model) {
+        return "main";
+    }
+}
